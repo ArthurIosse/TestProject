09ab29d2dc4c02a2dc04f255de6220ea1f8441eb
arthur <iosse.arthur@gmail.com>
2021-02-08 23:04:11 +0100

    feature-1 implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("src main");
     System.out.println("changes made...");
+    System.out.println("feature-1 added...");
   }
 }
